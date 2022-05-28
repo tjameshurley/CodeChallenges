9a2a629a0ca27f693bbd9221a8771b86a651eabe
Tim <33241796+tjameshurley@users.noreply.github.com>
2022-05-27 20:07:45 -0400

    Create Conditional2.java

---
+++ Conditional2.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Conditional2{
+
+    public Conditional2(){
+        
+    }
+
+    public void getAge(){
+       boolean foo = true;
+        while ( foo == true){
+            System.out.println("Please enter age: ");
+            Scanner scn = new Scanner(System.in);
+            int startAge = scn.nextInt();
+            
+             if( startAge >= 18){
+                foo = false;
+                }                      
+        }       
+    }
+
+
+    public static void main(String[] args){
+        Conditional2 ageTest1 = new Conditional2();
+        ageTest1.getAge();
+        
+
+    }
+
+}
